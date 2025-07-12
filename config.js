5bc1f0328dd9cf2f432da5c908eba48a8067a4b9
GittyHub2025 <AIstuffs2025@gmail.com>
2025-07-12 15:30:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,4 +7,4 @@
 // that you copied from the Google Apps Script deployment screen.
 
 
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzu783Bq6IOw8xdjBwJkXAHrYMCLwIapqTGYoKuBkevL7X4Ko3t6DgnG3f-1eOOFLsd/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxnjVuYoKDyU0S5lCVl1G-Dy4S0BN3HEhbC2JTgkI18kAlMIDaOgNcLOXHSMQRdVjU/exec';
